82a96ca6726e7eed4ee51befca18f3a6d4b530bb
TasciBerkan <65683218+TasciBerkan@users.noreply.github.com>
2022-05-24 15:53:07 +0300

    Add files via upload

---
+++ Sherlock and Array.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+    # Write your code here
+    tot = sum(arr)
+    add = 0
+    for element in arr:
+        if add == tot-element-add:
+            return "YES"
+        add+=element
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
